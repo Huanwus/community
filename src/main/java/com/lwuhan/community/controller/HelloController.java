823b3759bfa601b0846468f2e674ce0a70dabfd2
Huanwus <lwuhan@foxmail.com>
2019-08-16 23:57:25 +0800

    intit repo

---
+++ src/main/java/com/lwuhan/community/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.lwuhan.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Auther: wuhan
+ * @Date: 2019/8/16 23:30
+ * @Description:
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+
+    }
+}
